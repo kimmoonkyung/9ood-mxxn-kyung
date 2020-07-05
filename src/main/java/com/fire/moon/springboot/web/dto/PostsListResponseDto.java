b89b9b71ddb390e3c9e2eefb70f1ef0ab68754a6
kimmoonkyung <mxxnkyung@naver.com>
2020-07-05 21:49:07 +0900

    2020.07.05 mustache 입문 1일차 p124 ~ p151
    템플릿 엔진이란
    - 지정된 템플릿 양식과 데이터가 합쳐져 HTML 문서를 출력하는 소프트웨어를 야기함.
    - ex) JSP, Freemaker / React, VUE 등
    - 전자는 서버 템플릿 엔진이라 불리며 후자는 클라이언트 템플릿 엔진이라 불림.

---
+++ src/main/java/com/fire/moon/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.fire.moon.springboot.web.dto;
+
+import com.fire.moon.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
