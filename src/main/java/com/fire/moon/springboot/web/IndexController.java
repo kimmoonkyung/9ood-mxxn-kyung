c38d9e1c10b1cf8387ba2b202daee652b8968ae9
kimmoonkyung <mxxnkyung@naver.com>
2020-07-08 01:34:49 +0900

    2020.07.08 mustache 입문 2일차 p151 ~ p161 (게시글 수정, 삭제)
    게시글 등록, 수정, 삭제의 로직은 ApiController (Service Layer)
    웹 화면은 IndexController (Web Layer)
    - p101 참조.
    - 게시글 조회 화면과, 수정화면은 말 그대로 화면이 있기 때문에 IndexController에 화면용 매핑을 함.
    - 그러나 삭제는 수정화면에서 진행하기 때문에 (혹은 조회화면), 따로 삭제용 화면은 필요 없다.
    - 그러므로 IndexController에 삭제용 url을 매핑할 것이 없다.
    
    *삭제 로직
    - 화면에서 삭제버튼 클릭.
    - index.js의 delete함수 호출
    - delete 함수에서 ajax를 통해 매핑된 PostsApiController를 호출한다.
    - PostsApiController에 매핑된 함수로 PostsService의 delete 함수 호출.
    - PostsService의 delete 함수에서 PostsRepository(JPA Repository)가 지원하는 delete 메소드를 활용하여 삭제. [엔티티를 파라미터로 삭제 할 수도 있다 -> delete(entity)]
    - 나는 deleteById를 활용하여 삭제했다.
    
    더 공부하고 싶은데, 배분하는 날이라 모니터링과 공부를 겸하다보니 시간도 늦고 배도 고파서 이만..

--- src/main/java/com/fire/moon/springboot/web/IndexController.java
+++ src/main/java/com/fire/moon/springboot/web/IndexController.java
@@ -2,14 +2,21 @@ package com.fire.moon.springboot.web;
 
 
 import com.fire.moon.springboot.service.posts.PostsService;
+import com.fire.moon.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    /*
+        IndexController는 화면용이다.
+        **ApiController와 헷갈리지 않기.
+    */
 
     private final PostsService postsService;
 
@@ -19,11 +26,18 @@ public class IndexController {
         return "index";
     }
 
-
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+
 
 
 }
